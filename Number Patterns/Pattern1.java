c292914e16ee4760e55dd156c31f3f3ce00907f0
Vaishnavi Panse <140835509+vaishpanse@users.noreply.github.com>
2024-06-12 23:00:05 +0530

    Add files via upload

---
+++ Number Patterns/Pattern1.java
@@ -0,0 +1,28 @@
+package pattern.numbers;
+
+/*
+ *Input number = 181725
+ *Output
+ *55555
+ *22
+ *7777777
+ *1
+ *88888888
+ *1
+ */
+public class Pattern1 {
+	public static void main(String[] args) {
+		int num = 181725;
+		int r = 0;
+
+		while (num > 0) {
+			r = num % 10;
+			num = num / 10;
+
+			for (int i = 1; i <= r; i++) {
+				System.out.print(r);
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ Number Patterns/Pattern2.java
@@ -0,0 +1,28 @@
+package pattern.numbers;
+
+/*
+ *Input number = 987654
+ *Output
+ *444
+ *555
+ *666
+ *777
+ *888
+ *999
+ */
+public class Pattern2 {
+	public static void main(String[] args) {
+		int num = 987654;
+		int r = 0;
+		while (num > 0) {
+			r = num % 10;
+			num = num / 10;
+
+			for (int i = 1; i <= 3; i++) {
+				System.out.print(r);
+			}
+			System.out.println();
+		}
+	}
+
+}
